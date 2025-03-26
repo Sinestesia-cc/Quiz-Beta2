fad1fca840cdc2bb3a5aa3642c8e1cdfd38c9d10
JulioJulioso <98512612+JulioJulioso@users.noreply.github.com>
2025-03-26 10:22:10 -0300

    1.7 PWA
    PWA

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Sinestesia-Vipro-0.1.5 PWA";
+const contentToCache = [
+    "Build/Build PWA.loader.js",
+    "Build/Build PWA.framework.js",
+    "Build/Build PWA.data",
+    "Build/Build PWA.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
